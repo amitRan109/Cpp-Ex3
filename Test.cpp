ca6ce1df2183a12adff69f768a28031c5371558f
amitRan109 <57709369+amitRan109@users.noreply.github.com>
2020-04-28 16:53:34 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -7,10 +7,10 @@ bool isEqual(double a, double b) {
     return false;
 }
 
-TEST_CASE ("delete!!!") { 
-    RealVariable x;
-    for (int i=0; i<=70; i++) CHECK(isEqual(solve(x ==16 ),16));
-}
+// TEST_CASE ("delete!!!") { 
+//     RealVariable x;
+//     for (int i=0; i<=70; i++) CHECK(isEqual(solve(x ==16 ),16));
+// }
 
 TEST_CASE ("real: good") { //25
     RealVariable x;
@@ -69,6 +69,69 @@ TEST_CASE ("real:bad") { //20
     CHECK_THROWS(solve((x/0) == 9));
     CHECK_THROWS(solve((0/x) == 9));
     CHECK_THROWS(solve(((x+2)/0) == 9));
-
 }
 
+
+TEST_CASE ("real: good") { //25
+    RealVariable x;
+    CHECK(isEqual(solve(x ==22 ),22));
+    CHECK(isEqual(solve(3*x == 30),10));
+    CHECK(isEqual(solve(x == 8),8));
+    CHECK(isEqual(solve(5*x-4.0 == 46.0),10));
+    CHECK(isEqual(solve(8*x == 16),2));
+    CHECK(isEqual(solve(x+9 == 10),solve(x+6 == 7)));
+    CHECK(isEqual(solve(x+10 == 1),-9));
+    CHECK(isEqual(solve(x+11 == 0),-11));
+    CHECK(isEqual(solve(x+8 == 10),2));
+    CHECK(isEqual(solve(x-6 == 0),6));
+    CHECK(isEqual(solve(x-200 == 0),200));
+    CHECK(isEqual(solve(x-100 == 0),100));
+    CHECK(isEqual(solve(x-302 == 0),302));
+    CHECK(isEqual(solve(x+100 == 100),0));
+    CHECK(isEqual(solve(x+10 == 10),0));
+    CHECK(isEqual(solve(x+16 == 16),0));
+    CHECK(isEqual(solve(x+2 == 2),0));
+    CHECK(isEqual(solve(x+3 == 3),0));
+    CHECK(isEqual(solve(x+5 == 5),0));
+    CHECK(isEqual(solve(x+8 == 8),0));
+    CHECK(isEqual(solve(x+9 == 9),0));
+    CHECK(isEqual(solve(x+19 == 19),0));
+    CHECK(isEqual(solve(x+1000 == 1000),0));
+    CHECK(isEqual(solve(x+157 == 157),0));
+    CHECK(isEqual(solve(x+5555 == 5555),0));
+}
+
+TEST_CASE ("real:bad") { //30
+    RealVariable x;
+    RealVariable y;
+    CHECK_THROWS(solve(x^2 == x));
+    CHECK_THROWS(solve(y^2 == x));
+    CHECK_THROWS(solve((x^3)+9 == x^7));
+    CHECK_THROWS(solve((x^2) == -10));
+    CHECK_THROWS(solve((x^2) == -293));
+    CHECK_THROWS(solve((x^2) == -234));
+    CHECK_THROWS(solve((x^2) == -2));
+    CHECK_THROWS(solve((x^2) == -99));
+    CHECK_THROWS(solve((x^2) == -23));
+    CHECK_THROWS(solve((x^2) == -27));
+    CHECK_THROWS(solve((x^2) == -1));
+    CHECK_THROWS(solve((x^2) == -2));
+    CHECK_THROWS(solve((x^2) == -3));
+    CHECK_THROWS(solve((x^2) == -4));
+    CHECK_THROWS(solve((x^2) == -5));
+    CHECK_THROWS(solve((x^2) == -6));
+    CHECK_THROWS(solve((x^2) == -9));
+    CHECK_THROWS(solve((x^2) == -8));
+    CHECK_THROWS(solve((x^2) == -11));
+    CHECK_THROWS(solve((x^2) == -111));
+    CHECK_THROWS(solve((x^2) == -99989));
+    CHECK_THROWS(solve((x^2) == -55433));
+    CHECK_THROWS(solve((x^2) == -455));
+    CHECK_THROWS(solve((x^2) == -3331));
+    CHECK_THROWS(solve((x^2) == -33));
+    CHECK_THROWS(solve((x^2) == -89));
+    CHECK_THROWS(solve((x^2) == -33));
+    CHECK_THROWS(solve((x^2) == -45));
+    CHECK_THROWS(solve((x^2) == -31));
+    CHECK_THROWS(solve((x^2) == -3));
+}
